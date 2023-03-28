11283f22ed9e9fbc3677b314356123f58c2f430b
kunalmkv <kunalm4.kvtagore@gmail.com>
2023-03-28 07:31:10 +0530

    cart function

--- controllers/admin.js
+++ controllers/admin.js
@@ -30,7 +30,7 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product({ title: title, price: price, description: description, imageUrl: imageUrl });
+  const product = new Product({ title: title, price: price, description: description, imageUrl: imageUrl, userId: req.user });
   product
     .save()
     .then(result => {
@@ -99,7 +99,10 @@ exports.postEditProduct = (req, res, next) => {
 };*/
 exports.getProducts = (req, res, next) => {
   Product.find()
+    // .select('title price -_id')
+    // .populate('userId', 'name')
     .then(products => {
+      console.log(products)
       res.render('admin/products', {
         prods: products,
         pageTitle: 'Admin Products',
--- models/user.js
+++ models/user.js
@@ -11,9 +11,31 @@ const userSchema = mongoose.Schema({
         required: true
     },
     cart: {
-        items: [{ productId: { type: Schema.Types.ObjectId, required: true }, quantity: { type: Number, required: true } }]
+        items: [{ productId: { type: Schema.Types.ObjectId, ref: 'Product', required: true }, quantity: { type: Number, required: true } }]
     }
 })
+
+userSchema.methods.addToCart = function (product) {
+    {
+        const cartProductIndex = this.cart.items.findIndex(cp => {
+            return cp.productId == product._id.toString();
+        });
+        let newQuantity = 1;
+        const updatedCartItems = [...this.cart.items];
+        if (cartProductIndex >= 0) {
+            newQuantity = this.cart.items[cartProductIndex].quantity + 1;
+            updatedCartItems[cartProductIndex].quantity = newQuantity
+        }
+        else {
+            updatedCartItems.push({ productId: product._id, quantity: newQuantity })
+        }
+
+        const updatedCart = { items: updatedCartItems }
+        // console.log('addto cart****', updatedCart);
+        this.cart = updatedCart
+        return this.save()
+    }
+}
 
 module.exports = mongoose.model('User', userSchema);
 
